f2cfbefc14136e0b6c1f4ae920c9d1c5860cb42b
Dennis Koslowski <dennis.koslowski@gmx.de>
2024-08-20 18:05:42 +0200

    Real world calibration

--- src/calibrate_motors.py
+++ src/calibrate_motors.py
@@ -7,9 +7,7 @@ print("Calibrating willi's motors")
 
 willi = willi.Willi()
 
-try:
-    print("Press ctrl+c to interrupt")
-
+def calibrate_throttle():
     while True:
         s = input('Enter the throttle value (-1.0 to 1.0): ')
         try:
@@ -18,6 +16,28 @@ try:
             willi._set_throttle(throttle, throttle)
         except ValueError:
             print('Not a float: ', s)
+
+def calibrate_speed():
+    while True:
+        s = input('Enter the speed value in m/s (-1.0 to 1.0): ')
+        try:
+            speed = float(s)
+            print('Set speed to ', speed)
+            willi._set_throttle(speed, speed)
+        except ValueError:
+            print('Not a float: ', s)
+
+try:
+    print("Press ctrl+c to interrupt")
+    while True:
+        inp = input('Calibrate [t]hrottle or [s]peed? ')
+        if inp in ['t', 'T']:
+            calibrate_throttle()
+            break
+        elif inp in ['s', 'S']:
+            calibrate_speed()
+            break
+
 
 except KeyboardInterrupt:
     print("\nInterrpted")
